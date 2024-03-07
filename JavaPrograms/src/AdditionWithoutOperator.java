7089670a7fca09c7a1834847fac7187ed84b1ade
SalmaAarman <salma.mundaganur@gmail.com>
2024-03-07 13:00:13 +0000

    Addition without Operator Program Added

---
+++ JavaPrograms/src/AdditionWithoutOperator.java
@@ -0,0 +1,16 @@
+
+public class AdditionWithoutOperator {
+
+	public static void main(String[] args) 
+	{
+		int no1=2;
+		int no2=1;
+		for(int i=1;i<=no2;i++)
+		{
+			no1++;
+		}
+		System.out.println(no1);
+
+	}
+
+}
